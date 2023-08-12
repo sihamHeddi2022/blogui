77b5d44d1dad452594f5a3648d9748e9edb5c8f1
siham <sihamheddi945@gmail.com>
2023-08-12 12:59:01 +0100

    final version

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:3000/api/v1/'
+    baseURL: 'https://blog-rest-api-negt.onrender.com/api/v1/'
   });
